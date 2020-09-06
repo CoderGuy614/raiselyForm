aab49c8122733b5791aba80c11c259a0238e8d75
CoderGuy614 <jlutz.110@gmail.com>
2020-09-06 21:01:08 +0800

    Adjust Layout and Alignment

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Form, Container, Col, Row, InputGroup, Button } from "react-bootstrap";
+import {
+  Form,
+  Container,
+  Col,
+  Row,
+  InputGroup,
+  Button,
+  Alert,
+} from "react-bootstrap";
 import FormHeader from "./FormHeader";
 import { Formik } from "formik";
 import * as yup from "yup";
@@ -14,134 +22,129 @@ const schema = yup.object({
 
 const Signup = () => {
   return (
-    <Container className="w-50 mt-4">
-      <FormHeader />
-      <Formik
-        validationSchema={schema}
-        onSubmit={(values) => console.log(values)}
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          password: "",
-          password2: "",
-        }}
-      >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          values,
-          touched,
-          isValid,
-          errors,
-        }) => (
-          <Form noValidate onSubmit={handleSubmit}>
-            {/* First Row */}
-            <Form.Row>
-              <Form.Group as={Col} md="6" controlId="validationFormik01">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="firstName"
-                  placeholder="First Name..."
-                  value={values.firstName}
-                  onChange={handleChange}
-                  isValid={touched.firstName && !errors.firstName}
-                  isInvalid={!!errors.firstName}
-                />
-                <Form.Control.Feedback />
-                <Form.Control.Feedback type="invalid">
-                  {errors.email}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="6" controlId="validationFormik02">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="lastName"
-                  placeholder="Last Name..."
-                  value={values.lastName}
-                  onChange={handleChange}
-                  isValid={touched.lastName && !errors.lastName}
-                  isInvalid={!!errors.lastName}
-                />
+    <Container
+      className="d-flex justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <Row className="border border-primary rounded my-auto p-4 m-2">
+        <Formik
+          validationSchema={schema}
+          onSubmit={(values) => console.log(values)}
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            password2: "",
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            touched,
+            isValid,
+            errors,
+          }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              {/* First Row */}
+              <Form.Row>
+                <Form.Group as={Col} md="6" controlId="validationFormik01">
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name..."
+                    value={values.firstName}
+                    onChange={handleChange}
+                    isValid={touched.firstName && !errors.firstName}
+                    isInvalid={!!errors.firstName}
+                  />
+                  <Form.Control.Feedback />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.email}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="6" controlId="validationFormik02">
+                  <Form.Label>Last name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name..."
+                    value={values.lastName}
+                    onChange={handleChange}
+                    isValid={touched.lastName && !errors.lastName}
+                    isInvalid={!!errors.lastName}
+                  />
 
-                <Form.Control.Feedback />
-                <Form.Control.Feedback type="invalid">
-                  {errors.email}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row>
-            {/* Second Row */}
-            <Form.Row>
-              <Form.Group as={Col} md="12" controlId="validationFormik03">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Email Address..."
-                  name="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  isValid={touched.email && !errors.email}
-                  isInvalid={!!errors.email}
-                />
-                <Form.Control.Feedback />
-                <Form.Control.Feedback type="invalid">
-                  {errors.email}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} md="6" controlId="validationFormik04">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password..."
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  isValid={touched.password && !errors.password}
-                  isInvalid={!!errors.password}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.password}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="6" controlId="validationFormik05">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Confirm Password..."
-                  name="password2"
-                  value={values.password2}
-                  onChange={handleChange}
-                  isValid={touched.password2 && !errors.password2}
-                  isInvalid={!!errors.password2}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.password2}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row>
-            {/* CheckBox and Button Row */}
-            <Form.Row>
-              <Form.Group as={Col} md="12">
-                <Form.Check
-                  required
-                  name="terms"
-                  label="Agree to terms and conditions"
-                  onChange={handleChange}
-                  isInvalid={!!errors.terms}
-                  feedback={errors.terms}
-                  id="validationFormik0"
-                />
-              </Form.Group>
-              <Button type="submit">Submit form</Button>
-            </Form.Row>
-          </Form>
-        )}
-      </Formik>
+                  <Form.Control.Feedback />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.email}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+              {/* Second Row */}
+              <Form.Row>
+                <Form.Group as={Col} md="12" controlId="validationFormik03">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Email Address..."
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    isValid={touched.email && !errors.email}
+                    isInvalid={!!errors.email}
+                  />
+                  <Form.Control.Feedback />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.email}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md="6" controlId="validationFormik04">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password..."
+                    name="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    isValid={touched.password && !errors.password}
+                    isInvalid={!!errors.password}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.password}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="6" controlId="validationFormik05">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Confirm Password..."
+                    name="password2"
+                    value={values.password2}
+                    onChange={handleChange}
+                    isValid={touched.password2 && !errors.password2}
+                    isInvalid={!!errors.password2}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.password2}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+              {/* CheckBox and Button Row */}
+              <Form.Row>
+                <Button className="my-3" block type="submit">
+                  Submit form
+                </Button>
+              </Form.Row>
+            </Form>
+          )}
+        </Formik>
+      </Row>
     </Container>
   );
 };
